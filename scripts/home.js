d1aa8dc28ebb3dc571461cc758f4d8430654c8ae
fgstaffa <fgstaffa@bitbucket.org>
2020-05-25 22:24:43 -0300

    padroes atualizados 25052020

---
+++ scripts/home.js
@@ -0,0 +1,18 @@
+// function abrir(){
+//     alert('Bem-Vindo ao Love Rock! Melhor conteúdo de metal!' )
+// }
+
+var altFundo = document.getElementById('muda-fundo')
+
+
+
+function mudaFundo(){
+    altFundo.innerHTML = '<h1>Love Rock uma paixão para vida!</h1>   <p> Um site para demonstrar minha paixão pela música, especificamente pelo Heavy Metal geral.</p><p> Não sou especialista, mas tenho uma grande conhecimento de bandas e uma vivência muito grande em shows.</p>'
+    altFundo.style.opacity = '1'
+}
+
+function voltaF(){
+    altFundo.innerHTML = ''
+    altFundo.style.opacity = '0'
+}
+
