851f5adc56b974fff27b5d3e2fc8c4d1d4a5e21d
fgstaffa <fgstaffa@hotmail.com>
2020-06-02 19:01:31 -0300

    home e bandas finalizadas

--- scripts/home.js
+++ scripts/home.js
@@ -6,6 +6,7 @@ var altFundo = document.getElementById('muda-fundo')
 
 
 
+
 function mudaFundo(){
     altFundo.innerHTML = '<h1>Love Rock uma paixão para vida!</h1>   <p> Um site para demonstrar minha paixão pela música, especificamente pelo Heavy Metal geral.</p><p> Não sou especialista, mas tenho uma grande conhecimento de bandas e uma vivência muito grande em shows.</p>'
     altFundo.style.opacity = '1'
